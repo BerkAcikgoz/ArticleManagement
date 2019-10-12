c4b1ab310bf1e85eca2e19ee6feb7eedf52db520
BerkAcikgoz <berk.acikgoz1@gmail.com>
2019-10-12 22:40:14 +0300

    created project

---
+++ ArticleManagement.Api/Controllers/ArticleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleManagement.Api.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ArticleController : ControllerBase
+  {
+
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+   
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "value";
+    }
+
+ 
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+
+  }
+}
